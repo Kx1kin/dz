d5f919fb93176f627c201dca4cb2e94b783d5176
Kx1kin <142590045+Kx1kin@users.noreply.github.com>
2023-10-01 21:12:36 +1000

    DZ

--- advertisments/app_advertisments/admin.py
+++ advertisments/app_advertisments/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'update_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'update_date', 'auction', 'get_html_image']
     list_filter = ['auction', 'create_at']
     actions = ["make_auction_as_false", "make_auction_as_true"]
     fieldsets = (
         ("Общее", {
-            'fields': ('title', 'description')
+            'fields': ('title', 'description', 'image')
         }),
         (
             'Финансы', {
@@ -23,5 +23,6 @@ class AdvertisementAdmin(admin.ModelAdmin):
     @admin.action(description="Добавить возможность торга")
     def make_auction_as_true(self, request, queryset):
         queryset.update(auction = True)
+
 
 admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisments/app_advertisments/models.py
+++ advertisments/app_advertisments/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.utils.html import format_html
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisement(models.Model):
-     title = models.CharField("заголовок", max_length=128)
+     title = models.CharField("заголовок", max_length= 128)
      description = models.TextField("описание")
      price = models.DecimalField("цена", max_digits=10, decimal_places=2)
      auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
      create_at = models.DateTimeField(auto_now_add=True)
      update_at = models.DateTimeField(auto_now=True)
+     user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE, null=True)
+     image = models.ImageField("изображение", upload_to="advertisments/")
 
      @admin.display(description="Дата создания")
      def created_date(self):
@@ -18,7 +23,7 @@ class Advertisement(models.Model):
                return format_html(
                     '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', create_time
                )
-          return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+          return self.create_at.strftime("%d.%m.%Y в %H:%M:%S")
 
      @admin.display(description="Дата изменения")
      def update_date(self):
@@ -26,9 +31,17 @@ class Advertisement(models.Model):
           if self.update_at.date() == timezone.now().date():
                updated_time = self.update_at.time().strftime("%H:%M:%S")
                return format_html(
-                    '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time
+                    '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', updated_time
                )
           return self.update_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+     @admin.display(description='фото')
+     def get_html_image(self):
+          if self.image:
+               return format_html(
+                    '<img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
+               )
+
 
      def __str__(self):
           return f'Advertisements(id = {self.id}, title = {self.title}, price = {self.price})'
